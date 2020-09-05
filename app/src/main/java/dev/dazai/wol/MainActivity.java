472bc884b631a3ab5c1e8953c9ab80ccf77436a2
xazai <62793427+xazai@users.noreply.github.com>
2020-09-05 22:06:12 +0200

    Created NavigationView and Fragments.

--- app/src/main/java/dev/dazai/wol/MainActivity.java
+++ app/src/main/java/dev/dazai/wol/MainActivity.java
@@ -1,28 +1,89 @@
 package dev.dazai.wol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import java.util.List;
+import android.view.MenuItem;
+import android.view.View;
+import com.google.android.material.navigation.NavigationView;
+import dev.dazai.wol.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements NetworkScannerResults {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    ActivityMainBinding binding;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        NetworkScanner networkScanner = new NetworkScanner(getApplicationContext());
-        networkScanner.delegate = this;
-        //first ip, last ip, timeout
-        networkScanner.execute(0, 50, 200);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        navigationView = binding.navigationView;
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
 
+        if(savedInstanceState == null){
+            navigationView.setCheckedItem(navigationView.getMenu().getItem(0));
+            getSupportFragmentManager()
+                    .beginTransaction().
+                    replace((R.id.fragmentContainer), new DashboardFragment())
+                    .commit();
+        }
+
+        binding.menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.drawerLayout.openDrawer(GravityCompat.END);
+
+            }
+        });
+
+
+//        NetworkScanner networkScanner = new NetworkScanner(getApplicationContext());
+//        networkScanner.delegate = this;
+//        //first ip, last ip, timeout
+//        networkScanner.execute(0, 50, 200);
 
     }
 
     @Override
-    public void processFinish(List<String> networkResults) {
-        Log.i("Reachable devices", networkResults.toString());
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch(item.getItemId()){
+            case R.id.dashboard:
+                fragment = new DashboardFragment();
+                break;
+            case R.id.groups:
+                fragment = new MyGroupsFragment();
+                break;
+            case R.id.events:
+                fragment = new EventsFragment();
+                break;
+            case R.id.help:
+                fragment = new HelpFragment();
+                break;
+
+        }
+        binding.navigationView.setCheckedItem(item);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace((R.id.fragmentContainer), fragment)
+                .commit();
+        binding.drawerLayout.closeDrawer(GravityCompat.END);
+        return true;
     }
 
+
+
+    @Override
+    public void onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.END)){
+            binding.drawerLayout.closeDrawer(GravityCompat.END);
+        }else{
+            super.onBackPressed();
+        }
+
+    }
 
 }
 
