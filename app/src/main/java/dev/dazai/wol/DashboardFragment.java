0b6e990d2decee0e452547a6275e67e5855bb8f8
xazai <62793427+xazai@users.noreply.github.com>
2020-09-06 15:16:54 +0200

    Implement BottomSheetDialog and create a Layout of Dialog.

--- app/src/main/java/dev/dazai/wol/DashboardFragment.java
+++ app/src/main/java/dev/dazai/wol/DashboardFragment.java
@@ -8,16 +8,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.Objects;
 
 import dev.dazai.wol.databinding.ActivityMainBinding;
 import dev.dazai.wol.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
     FragmentDashboardBinding binding;
-
+    BottomSheetDialog bottomSheetDialog;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.newDeviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog = new BottomSheetDialog(Objects.requireNonNull(getActivity()), R.style.BottomSheetDialogTheme);
+                View bottomResultSheetView = LayoutInflater.from(getActivity()).inflate(
+                        R.layout.dashboard_new_device_dialog, (LinearLayout) Objects.requireNonNull(getView()).findViewById(R.id.dialog_container));
+                bottomSheetDialog.setContentView(bottomResultSheetView);
+                bottomSheetDialog.show();
+            }
+        });
 
 
     }
