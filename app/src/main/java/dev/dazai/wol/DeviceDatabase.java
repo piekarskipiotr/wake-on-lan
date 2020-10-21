0b4e99a5364cdebf1cb6ded8f78bfed6f5b0c980
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2020-10-21 21:56:46 +0200

    Add RoomDatabase.CallBack to the Database.

--- app/src/main/java/dev/dazai/wol/DeviceDatabase.java
+++ app/src/main/java/dev/dazai/wol/DeviceDatabase.java
@@ -1,27 +1,53 @@
 package dev.dazai.wol;
 
 import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Device.class}, version = 1)
 public abstract class DeviceDatabase extends RoomDatabase {
-    private static DeviceDatabase database;
+    private static DeviceDatabase instance;
     private static String DATABASE_NAME = "device_database";
+    public abstract DeviceDao deviceDao();
 
     public synchronized static DeviceDatabase getInstance(Context context){
-        if(database == null){
-            database = Room.databaseBuilder(context.getApplicationContext(),
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
                     DeviceDatabase.class, DATABASE_NAME)
-                    .allowMainThreadQueries()
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
-        return database;
+        return instance;
 
     }
 
-    public abstract DeviceDao deviceDao();
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private DeviceDao deviceDao;
+
+        private PopulateDbAsyncTask(DeviceDatabase deviceDatabase){
+            deviceDao = deviceDatabase.deviceDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+    }
+
+
 
 }
