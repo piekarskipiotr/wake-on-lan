3d23ef102aa0f8ed3279393f3daf67fb8247b6af
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2020-11-30 19:33:18 +0100

    Delete mac validation in MagicPacket.

--- app/src/main/java/dev/dazai/wol/MagicPacket.java
+++ app/src/main/java/dev/dazai/wol/MagicPacket.java
@@ -35,15 +35,11 @@ public class MagicPacket {
     private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
         byte[] bytes = new byte[6];
         String[] hex = macStr.split("(\\:|\\-)");
-        if (hex.length != 6) {
-            throw new IllegalArgumentException("Invalid MAC address.");
-        }
         try {
             for (int i = 0; i < 6; i++) {
                 bytes[i] = (byte) Integer.parseInt(hex[i], 16);
             }
-        }
-        catch (NumberFormatException e) {
+        }catch (NumberFormatException e) {
             throw new IllegalArgumentException("Invalid hex digit in MAC address.");
         }
         return bytes;
