f52e0673a9097e9817c29f3ef51c60af1b72d8d7
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2020-12-06 10:54:05 +0100

    Create ViewModel and Repository for MyGroupsFragment.

--- app/src/main/java/dev/dazai/wol/MyGroupsFragment.java
+++ app/src/main/java/dev/dazai/wol/MyGroupsFragment.java
@@ -5,26 +5,47 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
 
 import dev.dazai.wol.databinding.FragmentMyGroupsBinding;
 
 public class MyGroupsFragment extends Fragment implements GroupListAdapter.onNavigationArrowClick{
     FragmentMyGroupsBinding binding;
     GroupListAdapter adapter;
+    MyGroupsViewModel myGroupsViewModel;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         adapter = new GroupListAdapter(getContext(), MyGroupsFragment.this);
         binding.groupRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         binding.groupRecyclerView.setHasFixedSize(true);
         binding.groupRecyclerView.setAdapter(adapter);
+
+        myGroupsViewModel = ViewModelProviders.of(this).get(MyGroupsViewModel.class);
+        Group x = new Group();
+        x.setGroupName("Moja pierwsza grupa");
+        myGroupsViewModel.insert(x);
+        myGroupsViewModel.getAllGroups().observe(getViewLifecycleOwner(), new Observer<List<Group>>() {
+            @Override
+            public void onChanged(List<Group> groups) {
+                adapter.setGroups(groups);
+
+            }
+        });
+
+
+
 
 
 
