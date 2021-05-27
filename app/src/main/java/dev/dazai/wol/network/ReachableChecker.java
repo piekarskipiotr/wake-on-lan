5e38351728b4889993441fb5732d1c74b4d26720
Piotr Piekarski <ppiekarski8@gmail.com>
2021-05-27 14:32:39 +0200

    Remove unused code that throws error

--- app/src/main/java/dev/dazai/wol/network/ReachableChecker.java
+++ app/src/main/java/dev/dazai/wol/network/ReachableChecker.java
@@ -9,8 +9,8 @@ import java.lang.ref.WeakReference;
 import java.net.InetAddress;
 import java.util.List;
 
-import dev.dazai.wol.utilities.RunDeviceDialog;
 import dev.dazai.wol.data.Device;
+import dev.dazai.wol.utilities.RunDeviceDialog;
 import dev.dazai.wol.viewmodels.RunDeviceDialogViewModel;
 
 public class ReachableChecker extends AsyncTask<Void, Integer, Void> {
@@ -19,16 +19,15 @@ public class ReachableChecker extends AsyncTask<Void, Integer, Void> {
     private static final int REACHABLE_INT = -1;
     private RunDeviceDialogViewModel runDeviceDialogViewModel;
     private WeakReference<RunDeviceDialog> weakReference;
-    private boolean preDeviceReachableStatus = false;
     private boolean currentDeviceReachableStatus = false;
 
-    public ReachableChecker(RunDeviceDialog activity, Device device, RunDeviceDialogViewModel runDeviceDialogViewModel){
+    public ReachableChecker(RunDeviceDialog activity, Device device, RunDeviceDialogViewModel runDeviceDialogViewModel) {
         mDevice = device;
         weakReference = new WeakReference<>(activity);
         this.runDeviceDialogViewModel = runDeviceDialogViewModel;
     }
 
-    public ReachableChecker(RunDeviceDialog activity, List<Device> devices, RunDeviceDialogViewModel runDeviceDialogViewModel){
+    public ReachableChecker(RunDeviceDialog activity, List<Device> devices, RunDeviceDialogViewModel runDeviceDialogViewModel) {
         mDevices = devices;
         weakReference = new WeakReference<>(activity);
         this.runDeviceDialogViewModel = runDeviceDialogViewModel;
@@ -41,55 +40,44 @@ public class ReachableChecker extends AsyncTask<Void, Integer, Void> {
         weakReference.get().binding.headerText.setText("Sprawdzanie połączenia");
     }
 
-
     @Override
     protected Void doInBackground(Void... voids) {
-        if(mDevices != null){
+        if (mDevices != null) {
             int size = mDevices.size();
-            for(int i = 0; i < size; i++) {
-                if (mDevices.get(i).getReachable() != null) {
-                    preDeviceReachableStatus = mDevice.getReachable();
-                }
-                for(int ii = 1; ii < 50; ii++) {
-                    if(ii == 1)
+            for (int i = 0; i < size; i++) {
+                for (int ii = 1; ii < 50; ii++) {
+                    if (ii == 1)
                         publishProgress(ii);
 
-                    if(ii % 10 == 0)
+                    if (ii % 10 == 0)
                         publishProgress(ii / 10 + 1);
 
-                    try{
-                        if(isCancelled()){
+                    try {
+                        if (isCancelled()) {
                             break;
                         }
 
                         currentDeviceReachableStatus = InetAddress.getByName(mDevices.get(i).getDeviceIpAddress()).isReachable(1000);
                         if (currentDeviceReachableStatus) {
                             publishProgress(REACHABLE_INT);
-                            if(size - 1 == i)
+                            if (size - 1 == i)
                                 break;
                         }
-
-
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-
-
             }
-        }else{
-            if (mDevice.getReachable() != null) {
-                preDeviceReachableStatus = mDevice.getReachable();
-            }
+        } else {
             for (int ii = 1; ii < 50; ii++) {
-                if(ii == 1)
+                if (ii == 1)
                     publishProgress(ii);
 
-                if(ii % 10 == 0)
+                if (ii % 10 == 0)
                     publishProgress(ii / 10 + 1);
 
                 try {
-                    if(isCancelled()) {
+                    if (isCancelled()) {
                         break;
                     }
 
@@ -106,19 +94,18 @@ public class ReachableChecker extends AsyncTask<Void, Integer, Void> {
             }
         }
 
-
         return null;
     }
 
     @Override
     protected void onProgressUpdate(Integer... integers) {
         super.onProgressUpdate(integers);
-        if(integers[0] == -1){
+        if (integers[0] == -1) {
             mDevice.setReachable(currentDeviceReachableStatus);
             runDeviceDialogViewModel.update(mDevice);
             weakReference.get().adapter.setDevice(mDevice);
-        }else
-            weakReference.get().binding.headerText.setText("Sprawdzanie połączenia: próba "+integers[0]+"/5");
+        } else
+            weakReference.get().binding.headerText.setText("Sprawdzanie połączenia: próba " + integers[0] + "/5");
 
     }
 
@@ -132,9 +119,9 @@ public class ReachableChecker extends AsyncTask<Void, Integer, Void> {
                 new CountDownTimer(5000, 1000) {
                     public void onTick(long millisUntilFinished) {
                         int sec = (int) ((millisUntilFinished / 1200));
-                        weakReference.get().binding.headerText.setText("Automatyczne zamknięcie okna za "+ sec +"s");
-
+                        weakReference.get().binding.headerText.setText("Automatyczne zamknięcie okna za " + sec + "s");
                     }
+
                     public void onFinish() {
                         weakReference.get().binding.doneButton.setClickable(false);
                         weakReference.get().alertDialog.dismiss();
@@ -142,7 +129,7 @@ public class ReachableChecker extends AsyncTask<Void, Integer, Void> {
                     }
                 }.start();
             }
-        },4000);
+        }, 4000);
 
 
     }
